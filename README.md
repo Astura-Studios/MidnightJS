0bacbca70dafaf6309d1892ed30517eba126a46a
Ascendus <ascendus.dev@gmail.com>
2021-07-21 17:04:32 +1200

    Initialized repository

---
+++ README.md
@@ -0,0 +1,3 @@
+# MidnightJS - Redefining Bot Development
+
+MidnightJS is a simple NodeJS framework for the NodeJS Discord API wrapper Eris, which makes bot development easier for developers.
---
+++ types/index.d.ts
@@ -0,0 +1,167 @@
+/**
+ * A simple NodeJS framework for the NodeJS Discord API wrapper Eris, which makes bot development easier for developers.
+ * @author Midnight Development <contact.midnightdev@gmail.com>
+ * @copyright
+ * @license Apache-2.0
+ * @version 1.0.0
+ */
+declare module "midnight.js" {
+  import {
+    AllowedMentions,
+    Client,
+    Command,
+    Constants,
+    Message as ErisMessage,
+  } from "eris";
+  import { Collection } from "@discordjs/collection";
+  import { EventEmitter } from "events";
+
+  // Package Information
+  export const author: string;
+  export const contributors: string[];
+  export const description: string;
+  export const homepage: string;
+  export const keywords: string[];
+  export const license: string;
+  export const maintainers: string[];
+  export const name: string;
+  export const version: string;
+
+  // Options
+  namespace Options {
+    // Bot Related
+    export interface BotOptions {
+      allowedMentions?: AllowedMentions;
+      database?: boolean;
+      description?: string;
+      defaultImageFormat?: Types.ImageFormat;
+      defaultImageSize?: number;
+      filterMessages?: boolean;
+      getAllUsers?: boolean;
+      owners: string[];
+      platform?: Types.BotPlatform;
+      prefix: string | string[];
+      sharding?: boolean;
+      token: string;
+    }
+
+    export interface BotStatusOptions {
+      status?: Types.BotStatus;
+      text: string;
+      type: Types.BotActivity;
+      url?: string;
+    }
+
+    // Command Related
+    export interface ArgumentOptions {
+      description: string;
+      name: string;
+      prompt: {}; // Create interface for this
+      required: boolean;
+      type: string; // Create type for this
+      value: string;
+    }
+
+    export interface CommandOptions {
+      // Information
+      aliases?: string | string[];
+      category?: string;
+      description?: string;
+      examples?: string | string[];
+      name: string;
+      usage?: string;
+
+      // Restrictions
+      allowBots?: boolean;
+      allowedChannels?: Types.CommandChannel;
+      clientPermissions?:
+        | keyof Constants["Permissions"]
+        | (keyof Constants["Permissions"])[];
+      cooldownLength?: number;
+      ownerOnly?: boolean;
+      userPermissions?:
+        | keyof Constants["Permissions"]
+        | (keyof Constants["Permissions"])[];
+
+      // Restriction Exceptions
+      ignoreCooldowns?: string | string[];
+      ignorePermissions?: string | string[];
+
+      // Extra
+      args: Options.ArgumentOptions | Options.ArgumentOptions[]; // Create class for arguments later
+      prefix?: string | ((message: Structures.Message) => Promise<string>);
+    }
+  }
+
+  // Structures
+  namespace Structures {
+    class Command {
+      public constructor(bot: Bot, options: Options.CommandOptions);
+      public bot: Bot;
+
+      public aliases: string | string[];
+      public category: string;
+      public description: string;
+      public examples: string | string[];
+      public name: string;
+      public usage: string;
+
+      public allowBots: boolean;
+      public allowedChannels: Types.CommandChannel;
+      public clientPermissions:
+        | keyof Constants["Permissions"]
+        | (keyof Constants["Permissions"])[];
+      public cooldownLength: number;
+      public ownerOnly: boolean;
+      public userPermissions:
+        | keyof Constants["Permissions"]
+        | (keyof Constants["Permissions"])[];
+
+      public ignoreCooldowns: string | string[];
+      public ignorePermissions: string | string[];
+
+      public args: Options.ArgumentOptions | Options.ArgumentOptions[]; // Create class for arguments later
+      public prefix?:
+        | string
+        | ((message: Structures.Message) => Promise<string>);
+    }
+
+    class Message extends ErisMessage {
+      public bot: Bot;
+
+      public lineReply(content, options): Promise<any>;
+      public lineReplyNoMention(content, options): Promise<any>;
+    }
+  }
+
+  // Types
+  namespace Types {
+    // Bot Related
+    export type BotActivity =
+      | "watching"
+      | "listening"
+      | "playing"
+      | "streaming"
+      | "competing";
+    export type BotPlatform = "desktop" | "web" | "mobile";
+    export type BotStatus = "online" | "idle" | "dnd" | "offline";
+
+    // Command Related
+    export type CommandChannel = "guild" | "dm" | "all";
+
+    // Module Related
+    export type ImageFormat = "png" | "jpg" | "gif" | "webp";
+  }
+
+  // Classes
+  export class Bot {
+    public constructor(options?: Options.BotOptions);
+    private client: Client;
+    public commands: Collection<string, Structures.Command>;
+
+    public addCommand(options?: Options.CommandOptions): void;
+    public init(): void;
+    public setStatus(options: Options.BotStatusOptions): void;
+    public start(): Promise<void>;
+  }
+}
